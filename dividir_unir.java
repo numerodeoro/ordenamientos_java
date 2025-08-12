15b06cbae6a4389a113d1b5edfc41e34dbc4b61e
numerodeoro <numerodeoro@live.com>
2025-08-12 10:13:41 -0300

    borrando leftovers

--- dividir_unir.java
+++ dividir_unir.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Dividir_unir {
     public static void main(String[] args) {
         int[] listaArray = {1, 5, 3, 4, 5, 10, 7, 8, 9, 0, 10, 34, 45, 67};
-        // Convertir array a lista para trabajar mejor
+        
         List<Integer> lista = new ArrayList<>();
         for (int num : listaArray) {
             lista.add(num);
@@ -51,30 +51,3 @@ public class Dividir_unir {
     }
 }
     
-
-
-
-// def dividir_unir(lista):
-//     if len(lista) <= 1:
-//         return lista
-//     else:
-//         pos_centro = int(len(lista)/2)
-//         IZQ = dividir_unir(lista[:pos_centro])
-//         DER = dividir_unir(lista[pos_centro:])
-//         return unir(IZQ, DER)
-// def unir(lista1,lista2):
-//     unida = []
-//     cont1 = 0
-//     cont2 = 0
-//     while len(lista1)>=1 and len(lista2)>=1 and cont1<len(lista1) and cont2<len(lista2):
-//         if lista1[cont1]<=lista2[cont2]:
-//             unida.append(lista1[cont1])
-//             cont1+=1
-//         else:
-//             unida.append(lista2[cont2])
-//             cont2+=1
-//     if cont1==len(lista1) and cont2<len(lista2):
-//         unida=unida+lista2[cont2:]
-//     else:
-//         unida=unida+lista1[cont1:]
-//     return unida
